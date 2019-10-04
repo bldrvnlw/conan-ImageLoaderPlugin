059c5ccfb26acf40d790b46394feb29f335be5f9
Baldur van Lew <baldurvanlew@gmail.com>
2019-10-04 11:49:27 +0200

    Correct shared only filter

--- build.py
+++ build.py
@@ -5,7 +5,7 @@
 from bincrafters import build_template_default
 import os
 def _is_not_shared(build):
-    return build.options['hdps-core:shared'] == False
+    return build.options['ImageLoaderPlugin:shared'] == False
     
 if __name__ == "__main__":
 
