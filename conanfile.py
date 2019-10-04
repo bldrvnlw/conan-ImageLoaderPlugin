06bd3dd21bd83fd1ed013d7e422ee787b03c6bca
Baldur van Lew <baldurvanlew@gmail.com>
2019-10-04 13:29:00 +0200

    Fix bug in last commit

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile, CMake, tools
 import os
 import shutil
-
+import json
 
 class ImageloaderpluginConan(ConanFile):
     #TODO wrap with Conan build tools to extract version from source
